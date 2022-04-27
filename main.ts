2c918e9c547f4408403d92d158e12984ba45f288
mi-skam <maksim.codes@mailbox.org>
2022-04-27 18:23:32 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,30 @@
+let y = 0
+let x = 0
+y += 0
+for (let index = 0; index < 4; index++) {
+    led.plot(x, y)
+    x += 1
+    basic.pause(200)
+    led.unplot(x - 1, 0)
+}
+for (let index = 0; index < 4; index++) {
+    led.plot(x, y)
+    y += 1
+    basic.pause(200)
+    led.unplot(x, y - 1)
+}
+for (let index = 0; index < 4; index++) {
+    led.plot(x, y)
+    x += -1
+    basic.pause(200)
+    led.unplot(x + 1, y)
+}
+for (let index = 0; index < 4; index++) {
+    led.plot(x, y)
+    y += -1
+    basic.pause(200)
+    led.unplot(x, y + 1)
+}
+basic.forever(function () {
+    basic.showString("" + (input.soundLevel()))
+})
